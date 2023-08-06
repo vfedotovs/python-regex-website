c1039681a05da72ca4c9c37c088ee9029709ee99
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2023-08-06 17:46:25 +0100

    Initial add working website tested with no docker

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    regex_result = ""
+    if request.method == 'POST':
+        pattern = request.form['pattern']
+        text = request.form['text']
+        try:
+            regex_result = re.findall(pattern, text)
+            regex_result = ', '.join(regex_result)
+        except re.error:
+            regex_result = "Invalid regex pattern"
+    return render_template('index.html', regex_result=regex_result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
