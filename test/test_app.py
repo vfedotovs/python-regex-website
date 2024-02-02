8df52686dc41f5ed7d3e9c9a0f82a2a1c597e9be
Valentins Fedotovs <39173395+vfedotovs@users.noreply.github.com>
2024-02-02 23:26:09 +0000

    add test file and gitignore file

---
+++ test/test_app.py
@@ -0,0 +1,32 @@
+import pytest
+from flask import Flask
+from src.app import index  # Import your index function from your actual app file
+
+
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.config['TESTING'] = True
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_index_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_post_request_with_valid_pattern(client):
+    response = client.post('/', data=dict(pattern=r'\d+', text='123abc456'))
+    assert response.status_code == 200
+    assert b'123, 456' in response.data
+
+
+def test_post_request_with_invalid_pattern(client):
+    response = client.post('/', data=dict(pattern='[a-z+', text='123abc456'))
+    assert response.status_code == 200
+    assert b'Invalid regex pattern' in response.data
